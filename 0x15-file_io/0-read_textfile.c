403d1fb91b07e51512bcf3222af172d61d283d1e
liyaayalew <liyamekbib16@gmail.com>
2023-10-30 06:15:47 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - this reads a text file and prints it to the POSIX stdo
