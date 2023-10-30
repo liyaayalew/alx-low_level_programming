6a51bfb1e0641809c6228c1c614ab7206086b991
liyaayalew <liyamekbib16@gmail.com>
2023-10-30 07:19:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
  * create_file - prog creates a file 
@@ -14,30 +9,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int nletters;
-	int rwr;
+	int fd;
+	int w;
+	int len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	file_d = open(filename, 0CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (file_d == -1)
+	fd = open(filename, 0_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(file_d, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content(nletters); nletters++)
-		;
-
-	rwr = write(file_d, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(file_d);
+	close(fd);
 
 	return (1);
 }
